6f2cc193c6be957d07d647fb4e62e8f501249c02
dekanbro <dekanbrown@odyssy.io>
2022-11-14 13:00:42 -0700

    initial subscriber

--- contracts/subscriptions/Subscriptions.sol
+++ contracts/subscriptions/Subscriptions.sol
@@ -8,20 +8,37 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
 
 import "../interfaces/IBAAL.sol";
 
-// import "hardhat/console.sol";
-
+// Made for use with Baal(Molochv3)
+// allows a dao with a subscription model 
+// will grant shares in the dao for every payment
+// anyone can trigger a payment for a member 
+// - if their last payment was longer than the periodLength
+// - it will try to pay any missed payments from the last payment
+// a member is active if they have payed up to the last triggered payment
+// a member can cancel a subscription at anytime
+// time periods are based per member on their last payment
 contract SubscriptionShaman is ReentrancyGuard, Initializable {
-    event SubscriptionReceived(
+    event Subscription(
         address indexed contributorAddress,
         uint256 amount,
         address baal,
         uint256 lootToGive,
-        uint256 lootToPlatform
+        uint256 lootToPlatform,
+        uint256 starDate
     );
 
+    struct Subscriber {
+        uint256 id;
+        uint256 starDate;
+        uint256 lastPaymentDate;
+        uint256 streak;
+        bool isActive;
+    }
+
+    uint256 public priceActivation;
     uint256 public pricePerPeriod;
     uint256 public lootPerPeriod;
-    uint256 public expiery;
+    uint256 public periodLength;
     bool public shares;
 
     address[] public cuts;
@@ -30,33 +47,36 @@ contract SubscriptionShaman is ReentrancyGuard, Initializable {
     IBAAL public baal;
     IERC20 public token;
 
-    uint256 count;
-    mapping(uint256 => address) public subscribers;
-    mapping(address => bool) public isActive;
+    uint256 public count = 1;
+    Subscriber[] public subscribers;
+    mapping(address => uint256) public subscriberIdxs;
 
 
     constructor() initializer {}
 
     function init(
         address _moloch,
-        address payable _token, // use 0 address if token only 
+        address payable _token,
+        uint256 _priceActivation,
         uint256 _pricePer,
         uint256 _lootPerUnit,
-        uint256 _expiery,
+        uint256 _periodLength,
         bool _shares,
         address[] memory _cuts,
         uint256[] memory _amounts
     ) initializer external {
         baal = IBAAL(_moloch);
         token = IERC20(_token);
+        priceActivation = _priceActivation;
         pricePerPeriod = _pricePer;
         lootPerPeriod = _lootPerUnit;
-        expiery = _expiery;
+        periodLength = _periodLength;
         shares = _shares;
         cuts = _cuts;
         amounts = _amounts;
     }
 
+    // Mint tokens to and any extra cuts that are set
     function _mintTokens(address to) private {
         address[] memory _receivers = new address[](1);
         _receivers[0] = to;
@@ -72,139 +92,95 @@ contract SubscriptionShaman is ReentrancyGuard, Initializable {
 
         address[] memory _cutReceivers = new address[](cuts.length);
         for (uint256 i = 0; i < cuts.length; i++) {
-            _receivers[i] = cuts[i];
+            _cutReceivers[i] = cuts[i];
         }
 
         uint256[] memory _cutAmounts = new uint256[](amounts.length);
         for (uint256 i = 0; i < amounts.length; i++) {
-            _amounts[i] = amounts[i];
+            _cutAmounts[i] = amounts[i];
         }
 
         // mint loot to cuts
         baal.mintLoot(_receivers, _amounts);
     }
 
-    function subscribe() public {
-        // token needs unlimited approval
-        token.transferFrom(msg.sender, baal.target(), pricePerPeriod);
-        subscribers[count++] = msg.sender;
-        _mintTokens(msg.sender);
-        isActive[msg.sender] = true;
-    }
+    // new or subscription or resubscribe
+    // must be an inactive member
+    // transfers activation price to DAO
+    // transfers initial payment to dao
+    // mints initial tokens for 1 period
+    function subscribe() payable public {
+        // token needs token approval
 
-    function cancel() public {
-        isActive[msg.sender] = false;
-    }
+        uint256 idx = subscriberIdxs[msg.sender];
+        // 0 if new
+        require(idx == 0 || !subscribers[idx].isActive, "already active");
 
-    function triggerPayment(address subscriber) public {
-        // single sub
-        // reward
-    }
+        // send activation fee to dao
+        require(priceActivation == msg.value, "wrong activation fee");
+        (bool success, ) = baal.target().call{value: priceActivation}("");
+        require(success, "Transfer failed");
 
-    function onboarder20(uint256 _value) public nonReentrant {
-        require(address(baal) != address(0), "!init");
-        require(expiery > block.timestamp, "expiery");
+        // send first payment
+        bool success2 = token.transferFrom(msg.sender, baal.target(), pricePerPeriod);
+        require(success2, "erc20: Transfer failed");
 
-        require(baal.isManager(address(this)), "Shaman not manager");
-
-        require(_value % pricePerUnit == 0, "!valid amount"); // require value as multiple of units
-
-        uint256 numUnits = _value / pricePerUnit;
-
-        // send to dao
-        require(token.transferFrom(msg.sender, baal.target(), _value), "Transfer failed");
-
-        uint256 lootToGive = (numUnits * lootPerUnit);
-
-        _mintTokens(msg.sender, lootToGive);
+        if(idx == 0){
+            subscriberIdxs[msg.sender] = count;
+            subscribers.push(Subscriber(count, block.timestamp, block.timestamp, 1, true));
+            count++;
+        } else {
+            subscribers[idx - 1].isActive = true;
+            subscribers[idx - 1].lastPaymentDate = block.timestamp;
+            subscribers[idx - 1].streak = 1;
+        }
         
-        address[] memory _receivers = new address[](cuts.length);
-        for (uint256 i = 0; i < cuts.length; i++) {
-            _receivers[i] = cuts[i];
-        }
-
-        // loop to fill amount per cut
-        uint256 lootToCuts = 0;
-        uint256[] memory _amounts = new uint256[](amounts.length);
-        for (uint256 i = 0; i < amounts.length; i++) {
-            lootToCuts = lootToCuts + (amounts[i] * numUnits);
-            _amounts[i] = amounts[i] * numUnits;
-        }
-
-        // mint loot to cuts
-        baal.mintLoot(_receivers, _amounts);
-
-        // amount of loot? fees?
-        emit SubscriptionReceived(
-            msg.sender,
-            _value,
-            address(baal),
-            lootToGive,
-            lootToCuts
-        );
+        _mintTokens(msg.sender);
     }
+
+    // can cancel at anytime. no refunds for current period though
+    function cancel() public {
+        uint256 idx = subscriberIdxs[msg.sender] -1 ;
+        require(subscribers[idx].isActive == true, "not active");
+        subscribers[idx].isActive = false;
+        subscribers[idx].streak = 0;
+    }
+
+    // trigger a payment for an active subscriber
+    // back pay any miseed periods from last payment
+    function triggerPayment(address subscriber) public {
+        // pay all previous months and start a new payment date
+
+        uint256 idx = subscriberIdxs[subscriber] - 1;
+        require(subscribers[idx].isActive, "!active member");
+        uint256 timeFromLastPayment = block.timestamp - subscribers[idx].lastPaymentDate;
+
+        if(timeFromLastPayment > periodLength) {
+            
+            uint256 periodsToCollect = timeFromLastPayment/periodLength;
+
+            // todo: would be better to batch if possible
+            // especially on short periods
+            for (uint256 i = 0; i < periodsToCollect; i++) {
+                bool success = token.transferFrom(
+                    subscriber, 
+                    baal.target(), 
+                    pricePerPeriod);
+                if(!success){
+                    subscribers[idx].isActive = false;
+                    subscribers[idx].streak = 0;
+                    continue;
+                }
+                _mintTokens(subscriber);
+                subscribers[idx].streak++;
+            }
+            
+            subscribers[idx].lastPaymentDate = block.timestamp; 
+        }
+
+
+    }
+
 
 }
 
-contract OnboarderShamanSummoner {
-    address payable public template;
-
-    event SummonOnbShamanoarderComplete(
-        address indexed baal,
-        address onboarder,
-        address token,
-        uint256 pricePerUnit,
-        uint256 lootPerUnit,
-        uint256 expiery,
-        string details,
-        bool _shares,
-        address[] _cuts,
-        uint256[] _amounts
-    );
-
-    constructor(address payable _template) {
-        template = _template;
-    }
-
-    function summonOnboarder(
-        address _moloch,
-        address payable _token,
-        uint256 _pricePer,
-        uint256 _unitPerUnit,
-        uint256 _expiery,
-        string calldata _details,
-        bool _shares,
-        address[] memory _cuts,
-        uint256[] memory _amounts
-    ) public returns (address) {
-        OnboarderShaman onboarder = OnboarderShaman(payable(Clones.clone(template)));
-
-        onboarder.init(
-            _moloch,
-            _token,
-            _pricePer,
-            _unitPerUnit,
-            _expiery,
-            _shares,
-            _cuts,
-            _amounts    
-        );
-
-
-        emit SummonOnbShamanoarderComplete(
-            _moloch,
-            address(onboarder),
-            _token,
-            _pricePer,
-            _unitPerUnit,
-            _expiery,
-            _details,
-            _shares,
-            _cuts,
-            _amounts 
-        );
-
-        return address(onboarder);
-    }
-
-}
